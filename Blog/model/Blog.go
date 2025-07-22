a2f05ed2f47dc2b1786ea8202843e8024237219e
Anja Srdic <srdicanjaa31@gmail.com>
2025-07-22 21:01:32 +0200

    Liking of blogs enabled, fixed reading of jsob Rating

--- Blog/model/Blog.go
+++ Blog/model/Blog.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -23,6 +24,40 @@ type Blog struct {
 	Status      BlogStatus `json:"status"`
 	UserId      int64      `json:"userId"`
 	RatingSum   int        `json:"ratingSum"`
-	Ratings     []Rating   `json:"ratings" gorm:"type:jsonb"`
+	Ratings     Ratings    `json:"ratings" gorm:"type:jsonb"`
 	Comments    []Comment  `json:"comments" gorm:"foreignKey:BlogId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
+
+func (blog *Blog) AddRating(value int, userId int64) error {
+	for _, rating := range blog.Ratings {
+		if rating.UserID == userId {
+			return fmt.Errorf("Rating from this user already exists")
+		}
+	}
+	blog.Ratings = append(blog.Ratings, Rating{
+		UserID:    userId,
+		Value:     value,
+		CreatedAt: time.Now(),
+	})
+	blog.RecalculateRatingSum()
+	return nil
+}
+
+func (blog *Blog) RemoveRating(userId int64) {
+	newRatings := make([]Rating, 0)
+	for _, rating := range blog.Ratings {
+		if rating.UserID != userId {
+			newRatings = append(newRatings, rating)
+		}
+	}
+	blog.Ratings = newRatings
+	blog.RecalculateRatingSum()
+}
+
+func (blog *Blog) RecalculateRatingSum() {
+	sum := 0
+	for _, rating := range blog.Ratings {
+		sum += rating.Value
+	}
+	blog.RatingSum = sum
 }
