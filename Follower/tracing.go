fdb42027a5255a8f86dc9b8fe2fc24385f7201d3
Arijana Marić <maricarijana69@gmail.com>
2025-10-10 19:36:43 +0200

    monitoring

---
+++ Follower/tracing.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+)
+
+var serviceName = "follower-service"
+
+func initTracer() (*trace.TracerProvider, error) {
+	url := os.Getenv("JAEGER_ENDPOINT")
+	if len(url) > 0 {
+		return initJaegerTracer(url)
+	}
+	return initFileTracer()
+}
+
+func initJaegerTracer(url string) (*trace.TracerProvider, error) {
+	log.Printf("Initializing tracing to Jaeger at %s\n", url)
+	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(serviceName),
+		)),
+	)
+	otel.SetTracerProvider(tp)
+	return tp, nil
+}
+
+func initFileTracer() (*trace.TracerProvider, error) {
+	log.Println("Initializing tracing to traces.json")
+	f, err := os.Create("traces.json")
+	if err != nil {
+		return nil, err
+	}
+	exporter, err := stdouttrace.New(
+		stdouttrace.WithWriter(f),
+		stdouttrace.WithPrettyPrint(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	tp := trace.NewTracerProvider(trace.WithBatcher(exporter))
+	otel.SetTracerProvider(tp)
+	return tp, nil
+}
