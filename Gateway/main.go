6ee3850630cac282111b532d0ec599679119522c
Anja Srdic <srdicanjaa31@gmail.com>
2025-10-11 22:58:08 +0200

    logging tracing and monitoring of host machine enabled

--- Gateway/main.go
+++ Gateway/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"context"
-	follower "gateway/proto/follower"
 	"time"
 
 	// "gateway/proto/stakeholders"
+	"gateway/proto/routes"
 	stakeholders "gateway/proto/stakeholders"
 	"log"
 	"net"
@@ -22,19 +22,19 @@ import (
 )
 
 // CORS middleware
-	func withCORS(h http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200") // Angular app
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+func withCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200") // Angular app
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-        if r.Method == http.MethodOptions {
-            w.WriteHeader(http.StatusNoContent)
-            return
-        }
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
 
-        h.ServeHTTP(w, r)
-    })
+		h.ServeHTTP(w, r)
+	})
 }
 func main() {
 	// --- 1. Startujemo gRPC server ---
@@ -44,12 +44,6 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
-
-	// Ovde bi registrovao prave implementacije servisa
-	// ali gateway uglavnom ne implementira logiku,
-	// nego samo prosljeđuje, pa ovo možeš preskočiti
-	// ako gRPC server za Blog već radi u posebnom kontejneru.
-	// blog.RegisterBlogServiceServer(grpcServer, &YourBlogServerImpl{})
 
 	log.Println("Serving gRPC on 0.0.0.0:8080")
 	go func() {
@@ -67,16 +61,6 @@ func main() {
 		log.Fatalln("Failed to dial blog-service:", err)
 	}
 
-	// connStakeholders, err := grpc.DialContext(
-	//     context.Background(),
-	//     "stakeholders-service:80",
-	//     grpc.WithTransportCredentials(insecure.NewCredentials()),
-	//     grpc.WithBlock(),
-	// )
-	// if err != nil {
-	//     log.Fatalln("Failed to dial stakeholders-service:", err)
-	// }
-
 	// --- 3. REST gateway mux ---
 	gwmux := runtime.NewServeMux()
 
@@ -86,25 +70,6 @@ func main() {
 		log.Fatalln("Failed to register blog gateway:", err)
 	}
 
-	// if err := stakeholders.RegisterStakeholdersServiceHandler(context.Background(), gwmux, connStakeholders); err != nil {
-	//     log.Fatalln("Failed to register stakeholders gateway:", err)
-	// }
-	// log.Println("Dialing stakeholders-service...")
-	// connStakeholders, err := grpc.DialContext(
-	// 	context.Background(),
-	// 	"stakeholders-service:80",
-	// 	grpc.WithTransportCredentials(insecure.NewCredentials()),
-	// 	grpc.WithBlock(),
-	// )
-	// if err != nil {
-	// 	log.Fatalln("Failed to dial stakeholders-service:", err)
-	// }
-	// log.Println("Dial OK, registering handler...")
-
-	// if err := stakeholders.RegisterStakeholdersServiceHandler(context.Background(), gwmux, connStakeholders); err != nil {
-	// 	log.Fatalln("Failed to register stakeholders gateway:", err)
-	// }
-	// log.Println("Stakeholders handler registered successfully")
 	log.Println("Dialing stakeholders-service...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
@@ -127,32 +92,8 @@ func main() {
 		}
 	}
 
-	connFollower, err := grpc.DialContext(
-    context.Background(),
-    "follower-service:9091", // ime servisa iz docker-compose + port
-    grpc.WithTransportCredentials(insecure.NewCredentials()),
-    grpc.WithBlock(),
-		)
-		if err != nil {
-			log.Fatalln("Failed to dial follower-service:", err)
-		}
-
-		// Registruj Follower REST handler
-		err = follower.RegisterFollowerServiceHandler(context.Background(), gwmux, connFollower)
-		if err != nil {
-			log.Fatalln("Failed to register follower gateway:", err)
-		}
-
-	// log.Println("Dial OK, registering handler...")
-
-	// if err := stakeholders.RegisterStakeholdersServiceHandler(context.Background(), gwmux, connStakeholders); err != nil {
-	// 	log.Fatalln("Failed to register stakeholders gateway:", err)
-	// }
-
-	// 👉 ovde kasnije možeš dodati i druge servise:
-	// stakeholders.RegisterStakeholderServiceHandler(ctx, gwmux, conn2)
-	// orders.RegisterOrderServiceHandler(ctx, gwmux, conn3)
-	// itd.
+	// --- FOLLOWER SERVICE (REST proxy) ---
+	routes.RegisterFollowerRoutes(gwmux)
 
 	//TOURS SERVICE
 
@@ -173,10 +114,10 @@ func main() {
 		}
 
 		if err := tours.RegisterKeyPointServiceHandler(context.Background(), gwmux, connTours); err != nil {
-        log.Printf("Failed to register keypoints gateway: %v", err)
-    	} else {
-        log.Println("KeyPoints handler registered successfully")
-    	}
+			log.Printf("Failed to register keypoints gateway: %v", err)
+		} else {
+			log.Println("KeyPoints handler registered successfully")
+		}
 
 		if err := tours.RegisterTourReviewServiceHandler(context.Background(), gwmux, connTours); err != nil {
 			log.Printf("Failed to register tour reviews gateway: %v", err)
@@ -190,15 +131,11 @@ func main() {
 		}
 	}
 
-	
-
-
 	// --- 4. Start REST server ---
 	gwServer := &http.Server{
-    Addr:    ":8090",
-    Handler: withCORS(gwmux),
-}
-
+		Addr:    ":8090",
+		Handler: withCORS(gwmux),
+	}
 
 	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
 	log.Fatalln(gwServer.ListenAndServe())
